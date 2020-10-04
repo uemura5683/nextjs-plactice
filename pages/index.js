9f1a4e303d3dc38521920d13cef24ab7a0845ebe
uemuragame5683 <nobuyoshi5683@gmail.com>
2020-10-05 01:50:22 +0900

    next.js plactice

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,40 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link';
-export default function Home({blog}) {
+import { getSortedPostsData } from '../lib/posts'
+
+export default function Home({ allPostsData }) {
   return (
-    <div>
-      {blog.map(blog => (
-        <ul key={blog.id}>
-          <li>
-            <Link href={`blog/${blog.id}`}>
-              <a>{blog.title}</a>
-            </Link>
-          </li>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
         </ul>
-      ))}
-    </div>
-  );
+      </section>
+    </Layout>
+  )
 }
-export const getStaticProps = async () => {
-  let key = {
-    headers: {'X-API-KEY': 'a9ca1ec4-edff-43d8-ace7-e5f0c68b5b50'},
-  };
-  let data = await fetch('https://uemura5683.microcms.io/api/v1/blog', key)
-    .then(res => res.json())
-    .catch(() => null);
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
   return {
     props: {
-      blog: data.contents,
-    },
-  };
-};
+      allPostsData
+    }
+  }
+}
